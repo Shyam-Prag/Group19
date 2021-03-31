7c8b8f1a692651395d652f659cfd672bb994f917
SHERIFF NYATHI <siphelelenyathi52@gmail.com>
2021-03-31 05:55:09 +0200

    My first commit

---
+++ src/main/java/za/ac/cput/Group19/PasswordValidator.java
@@ -0,0 +1,20 @@
+package za.ac.cput.Group19;
+/**
+ * @author SIPHELELE NYATHI
+ * 218334028
+ * This class validates a password
+ */
+
+public class PasswordValidator {
+
+    /**
+     * This method checks if the password is valid and returns a boolean
+     */
+    public boolean isValid(String password){
+        if (password.length() >=8 && password.length() <=10)
+            return true;
+        else
+            return false;
+    }
+
+}
