fd007d219b5e009a171f86050fe1a4f06492f650
SHERIFF NYATHI <siphelelenyathi52@gmail.com>
2021-03-31 06:19:29 +0200

    My commit

--- src/main/java/za/ac/cput/Group19/PasswordValidator.java
+++ src/main/java/za/ac/cput/Group19/PasswordValidator.java
@@ -11,10 +11,11 @@ public class PasswordValidator {
      * This method checks if the password is valid and returns a boolean
      */
     public boolean isValid(String password){
-        if (password.length() >=5 && password.length() <=10)
+        if (password.length() >=5 && password.length() <=10) {
             return true;
-        else
+        } else {
             return false;
+        }
     }
 
 }
