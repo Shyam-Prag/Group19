3d3270089fa48491e2ff1f36eb101cdf8362337b
Cameron Noemdo <219115443@mycput.ac.za>
2021-04-01 12:38:58 +0200

    Added Calculate class

---
+++ src/main/java/za/ac/cput/Calculate.java
@@ -0,0 +1,24 @@
+package za.ac.cput;
+
+public class Calculate
+{
+    /**
+     * @author Cameron Noemdo 219115443
+     */
+    public int num;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return "Calculate{" +
+                "num=" + num +
+                '}';
+    }
+}
